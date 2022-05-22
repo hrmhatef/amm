cf0cac0a429c85b15ed8baca5a3f4a17ae7ee2ac
hatef <rahmani.hatef@gmail.com>
2022-05-23 03:43:16 +1000

    amm: add func swap
     - also update the unit test

--- amm/src/lib.rs
+++ amm/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp::max;
+
 use near_contract_standards::fungible_token::metadata::FungibleTokenMetadata;
 use near_contract_standards::fungible_token::FungibleToken;
 use near_sdk::{env, log, near_bindgen, AccountId, Balance, PanicOnDefault, PromiseOrValue};
@@ -6,7 +8,7 @@ use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::json_types::U128;
 
 mod utils;
-use utils::add_decimals;
+use utils::{add_decimals, calc_dy, remove_decimals};
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
@@ -117,6 +119,69 @@ impl AMM {
             ticker,
             &payer_id
         );
+    }
+
+    pub fn swap(
+        &mut self,
+        buy_token_name: AccountId,
+        sell_token_name: AccountId,
+        sell_amount: U128,
+    ) -> U128 {
+        if buy_token_name.eq(&sell_token_name) {
+            panic!("Tokens can't be equals")
+        }
+
+        self.check_meta();
+        let buy_token;
+        let sell_token;
+        if buy_token_name.eq(&self.account_id_token_a)
+            && sell_token_name.eq(&self.account_id_token_b)
+        {
+            buy_token = &mut self.token_a;
+            sell_token = &mut self.token_b;
+        } else if buy_token_name.eq(&self.account_id_token_b)
+            && sell_token_name.eq(&self.account_id_token_a)
+        {
+            buy_token = &mut self.token_b;
+            sell_token = &mut self.token_a;
+        } else {
+            panic!("Token not supported");
+        }
+        let pool_owner_id = env::current_account_id();
+        let user_account_id = env::predecessor_account_id();
+
+        // Get current statement of pool
+        let x = sell_token.0.internal_unwrap_balance_of(&pool_owner_id);
+        let y = buy_token.0.internal_unwrap_balance_of(&pool_owner_id);
+
+        // Send sell_tokens to pool from seller
+        sell_token
+            .0
+            .internal_transfer(&user_account_id, &pool_owner_id, sell_amount.0, None);
+
+        // Convert to the same decimal
+        let max_decimals = max(
+            buy_token.1.clone().unwrap().decimals,
+            sell_token.1.clone().unwrap().decimals,
+        );
+        let x = add_decimals(x, max_decimals - sell_token.1.clone().unwrap().decimals);
+        let y = add_decimals(y, max_decimals - buy_token.1.clone().unwrap().decimals);
+
+        // Calc buy amount
+        let buy_amount = calc_dy(x, y, sell_amount.0);
+
+        // Restore decimal
+        let buy_amount = remove_decimals(
+            buy_amount,
+            max_decimals - buy_token.1.clone().unwrap().decimals,
+        );
+
+        // Send buy value to user buyer
+        buy_token
+            .0
+            .internal_transfer(&pool_owner_id, &user_account_id, buy_amount, None);
+
+        U128::from(buy_amount)
     }
 
     fn token_a(&self) -> Option<&TokenInfo> {
@@ -322,5 +387,34 @@ mod tests {
         let amount = 10_000_u128;
         let mut amm = AMM::new(owner, token_rick.clone(), token_morty.clone());
         amm.add_token_to_pool(token_rick, amount.into(), None);
+    }
+
+    #[test]
+    #[should_panic(expected = "Tokens can't be equals")]
+    fn test_swap_same_tokens() {
+        let context = get_context(accounts(0));
+        testing_env!(context.build());
+        let owner = accounts(1);
+        let token_rick = accounts(2);
+        let token_morty = accounts(3);
+        let amount = 10_000_u128;
+        let mut amm = AMM::new(owner, token_rick.clone(), token_morty);
+        amm.swap(token_rick.clone(), token_rick, amount.into());
+    }
+
+    #[test]
+    #[should_panic(expected = "Token not supported")]
+    fn test_swap_not_supported() {
+        let context = get_context(accounts(0));
+        testing_env!(context.build());
+        let owner = accounts(1);
+        let token_rick = accounts(2);
+        let token_morty = accounts(3);
+        let token_zombie = accounts(4);
+        let amount = 10_000_u128;
+        let mut amm = AMM::new(owner, token_rick.clone(), token_morty);
+        amm.set_metadata_a(meta_a());
+        amm.set_metadata_b(meta_b());
+        amm.swap(token_rick, token_zombie, amount.into());
     }
 }
