bcd6c05a02ab2aea9c376b7575090250a1ef42f6
hatef <rahmani.hatef@gmail.com>
2022-05-23 20:28:16 +1000

    sim: edit to use new amm file

--- tests/sim/test.rs
+++ tests/sim/test.rs
@@ -5,7 +5,7 @@ use near_sdk_sim::{call, to_yocto, view};
 #[test]
 fn simulate_total_supply() {
     let initial_balance = to_yocto("100");
-    let (_, ft, _) = init(initial_balance);
+    let (_, ft, _, _, _) = init(initial_balance);
     let total_supply: U128 = view!(ft.ft_total_supply()).unwrap_json();
     assert_eq!(initial_balance, total_supply.0);
 }
@@ -14,7 +14,7 @@ fn simulate_total_supply() {
 fn simulate_simple_transfer() {
     let transfer_amount = to_yocto("100");
     let initial_balance = to_yocto("100000");
-    let (root, ft, rick) = init(initial_balance);
+    let (root, ft, _, _, rick) = init(initial_balance);
 
     // Transfer from root to rick.
     call!(
@@ -24,7 +24,7 @@ fn simulate_simple_transfer() {
     )
     .assert_success();
     let root_balance: U128 = view!(ft.ft_balance_of(root.account_id())).unwrap_json();
-    let alice_balance: U128 = view!(ft.ft_balance_of(rick.account_id())).unwrap_json();
+    let rick_balance: U128 = view!(ft.ft_balance_of(rick.account_id())).unwrap_json();
     assert_eq!(initial_balance - transfer_amount, root_balance.0);
-    assert_eq!(transfer_amount, alice_balance.0);
+    assert_eq!(transfer_amount, rick_balance.0);
 }
--- tests/sim/utils.rs
+++ tests/sim/utils.rs
@@ -1,14 +1,18 @@
+use amm::AMMContract;
 use ft::FTContractContract as FTContract;
 
 use near_contract_standards::fungible_token::metadata::{FungibleTokenMetadata, FT_METADATA_SPEC};
 use near_sdk::serde_json::json;
 use near_sdk_sim::{deploy, init_simulator, to_yocto, ContractAccount, UserAccount};
 
-pub const FT_ID: &str = "token";
+pub const FT_A_ID: &str = "token_a";
+pub const FT_B_ID: &str = "token_b";
+pub const AMM_ID: &str = "amm";
 
 // Load in contract bytes at runtime
 near_sdk_sim::lazy_static_include::lazy_static_include_bytes! {
     FT_WASM_BYTES => "res/ft.wasm",
+    AMM_WASM_BYTES => "res/amm.wasm",
 }
 
 // Register the given `user` with FT contract
@@ -27,33 +31,82 @@ pub fn register_user(contract_id: &str, user: &near_sdk_sim::UserAccount) {
     .assert_success();
 }
 
-pub fn init(initial_balance: u128) -> (UserAccount, ContractAccount<FTContract>, UserAccount) {
-    let root = init_simulator(None);
-    let meta = FungibleTokenMetadata {
+pub fn meta_a() -> FungibleTokenMetadata {
+    FungibleTokenMetadata {
         spec: FT_METADATA_SPEC.to_string(),
-        name: "FT".to_string(),
-        symbol: "FTS".to_string(),
+        name: "Fungible token B".to_string(),
+        symbol: "FTA".to_string(),
         icon: None,
         reference: None,
         reference_hash: None,
         decimals: 3,
-    };
+    }
+}
 
-    // Init Token contract
-    let token_contract = deploy!(
+pub fn meta_b() -> FungibleTokenMetadata {
+    FungibleTokenMetadata {
+        spec: FT_METADATA_SPEC.to_string(),
+        name: "Fungible token A".to_string(),
+        symbol: "FTB".to_string(),
+        icon: None,
+        reference: None,
+        reference_hash: None,
+        decimals: 3,
+    }
+}
+pub fn init(
+    initial_balance: u128,
+) -> (
+    UserAccount,
+    ContractAccount<FTContract>,
+    ContractAccount<FTContract>,
+    ContractAccount<AMMContract>,
+    UserAccount,
+) {
+    let root = init_simulator(None);
+    // Init Token A contract
+    let token_a_contract = deploy!(
         contract: FTContract,
-        contract_id: FT_ID,
+        contract_id: FT_A_ID,
         bytes: &FT_WASM_BYTES,
         signer_account: root,
         init_method: new(
             root.account_id(),
             initial_balance.into(),
-            meta.clone()
+            meta_a()
+        )
+    );
+    // Init Token B contract
+    let token_b_contract = deploy!(
+        contract: FTContract,
+        contract_id: FT_B_ID,
+        bytes: &FT_WASM_BYTES,
+        signer_account: root,
+        init_method: new(
+            root.account_id(),
+            initial_balance.into(),
+            meta_b()
         )
     );
 
     let rick = root.create_user("rick".parse().unwrap(), to_yocto("100"));
-    register_user(FT_ID, &rick);
+    register_user(FT_A_ID, &rick);
+    register_user(FT_B_ID, &rick);
 
-    (root, token_contract, rick)
+    // Init AMM contract
+    let amm_contract = deploy!(
+        contract: AMMContract,
+        contract_id: AMM_ID,
+        bytes: &AMM_WASM_BYTES,
+        signer_account: root,
+        init_method: new(
+            AMM_ID.parse().unwrap(),
+            token_a_contract.account_id(),
+            token_b_contract.account_id()
+        )
+    );
+    register_user(FT_A_ID, &amm_contract.user_account);
+    register_user(FT_B_ID, &amm_contract.user_account);
+
+    (root, token_a_contract, token_b_contract, amm_contract, rick)
 }
